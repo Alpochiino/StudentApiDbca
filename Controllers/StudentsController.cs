8a8fd2c482c061c0e1369dfb1cd09818cd1f625a
Christian Alp <Chrillealp11@hotmail.com>
2025-01-12 19:16:21 +0100

    Update

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,30 @@
+namespace StudentApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using StudentApi.Data;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StudentsController : ControllerBase
+{
+	private readonly StudentDbContext _context;
+
+	public StudentsController(StudentDbContext context)
+	{
+		_context = context;
+	}
+	
+	// GET: api/students
+	[HttpGet]
+	public async Task<IActionResult> GetStudents()
+	{
+		var students = await _context.Students.ToListAsync();
+		return Ok(students);
+	}
+}
