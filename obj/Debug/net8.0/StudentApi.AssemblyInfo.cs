ebeaeda606bc1fc11e3acad4a7dbc26bcfcc008d
Christian Alp <Chrillealp11@hotmail.com>
2025-01-13 16:11:39 +0100

    Update

--- obj/Debug/net8.0/StudentApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9155e132d4d72ca9507b51b536b9840ef01dd66c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde3489954a2377603aeca562735c5422ab0dfb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
